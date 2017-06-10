a28cb00f3ace1a3ff476342bcda0496811d0ca43
Silviu Eigel <silviu.eigel@thinslices.com>
2017-06-10 17:18:56 +0300

    implement Eval method for all structs

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,15 +1,39 @@
 package calculator
 
+import (
+	"fmt"
+	"math"
+)
+
 type Expr interface {
+	Eval(env Env) float64
 }
 
 type Var string
 
+func (v Var) Eval(env Env) float64 {
+	return env[v]
+}
+
 type literal float64
+
+func (l literal) Eval(env Env) float64 {
+	return float64(l)
+}
 
 type unary struct {
 	op rune
 	x  Expr
+}
+
+func (u unary) Eval(env Env) float64 {
+	switch u.op {
+	case '+':
+		return +u.x.Eval(env)
+	case '-':
+		return -u.x.Eval(env)
+	}
+	panic(fmt.Sprintf("unsupported unary operator: %q", u.op))
 }
 
 type binary struct {
@@ -17,7 +41,35 @@ type binary struct {
 	x, y Expr
 }
 
+func (b binary) Eval(env Env) float64 {
+	switch b.op {
+	case '+':
+		return b.x.Eval(env) + b.y.Eval(env)
+	case '-':
+		return b.x.Eval(env) - b.y.Eval(env)
+	case '*':
+		return b.x.Eval(env) * b.y.Eval(env)
+	case '/':
+		return b.x.Eval(env) / b.y.Eval(env)
+	}
+	panic(fmt.Sprintf("unsupported binary operator: %q", b.op))
+}
+
 type call struct {
 	fn   string
-	args []Expr–
+	args []Expr
 }
+
+func (c call) Eval(env Env) float64 {
+	switch c.fn {
+	case "pow":
+		return math.Pow(c.args[0].Eval(env), c.args[1].Eval(env))
+	case "sin":
+		return math.Sin(c.args[0].Eval(env))
+	case "sqrt":
+		return math.Sqrt(c.args[0].Eval(env))
+	}
+	panic(fmt.Sprintf("unsupported call: %q", c.fn))
+}
+
+type Env map[Var]float64
