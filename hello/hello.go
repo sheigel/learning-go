def2c9116d5b07b52a0fa65a73f892f13cf15e14
Silviu Eigel <silviu.eigel@thinslices.com>
2017-06-24 17:02:50 +0300

    playing around with interfaces and pointers

--- hello/hello.go
+++ hello/hello.go
@@ -1,6 +1,8 @@
 package main
 
-import "time"
+import (
+	"fmt"
+)
 
 var a string
 
@@ -12,15 +14,45 @@ func f(asdf string) {
 func hello() {
 	a = "hello, world"
 	go f(a)
-	a="haaaa"
+	a = "haaaa"
 }
 
+type str string
+
+func (s *str) Write(p []byte) (n int, err error) {
+	return 10, nil
+}
+
+func (s str) Read(p []byte) (n int, err error) {
+	return 11, nil
+}
+
+func (s str) String() string {
+	return fmt.Sprintf("test:%v", string(s))
+}
 
 func main() {
+	//test(str("direct string"))
+	//test(nil)
+	//test(new(str))
+	//var s str
+	//test(s)
+	//s := str("asdfk")
+	rw := str("asdf")
 
-	hello()
+	var val int
+	val, _ = (&rw).Write([]byte("qqqq"))
+	println(val)
+	val, _ = (&rw).Read([]byte("qqqq"))
+	println(val)
+}
 
-	time.Sleep(3*time.Second)
+func test(s fmt.Stringer) {
+	println("is nil", s == nil)
+	if s == nil {
+		return
+	}
+	println("string is", s.String())
 }
 
 func receiver(source chan int, completed chan interface{}) {
